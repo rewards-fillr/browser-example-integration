beb35e2311ecb515f9eaa657152ca801f33877c0
JK Jung <kernel24@gmail.com>
2018-10-02 15:48:53 +0900

    remove key

--- index.ts
+++ index.ts
@@ -8,8 +8,8 @@ import ProfileData from './profile-german'; // see full profile example data
 //   "PersonalDetails.LastName": "Wick",
 // }
 
-const devKey = 'ab14afe7b1b4befaf6e52d5f24fa90e8';  // Set your dev key
-const secretKey = 'Y2VhZGVmMDhkZWZiZWM1NWFhM2U4NjI='; // Set your secret key
+const devKey = '';  // Set your dev key
+const secretKey = ''; // Set your secret key
 const profileDataHandler = new ProfileDataInterface((mappings) => {
   mappings.profile = ProfileData; // Set your profile data
   fillr.performFill(mappings);
